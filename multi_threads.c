2d99be596c16147284958cde603d31cf4321f5f5
Windsao <swu23@gwu.edu>
2019-09-26 15:41:56 -0400

    change

--- multi_threads.c
+++ multi_threads.c
@@ -2,26 +2,31 @@
 #include <pthread.h>
 #include "fibonacci.h"
 
-int result[10];
+int result;
 
 void* passArg(void* n){
-    int result = fibonacci(*(int*)n);
+    result = fibonacci(*(int*)n);
     return (void*)&result;
 }
 
 int main(void){
     void* status;
     int n = 10;
+    int values[10];
     pthread_t thread[10];
+    for(int i = 0; i < 10; i++)
+    	values[i] = i;
+    
     for (int i = 0; i < n; i++)
-	if (pthread_create(&thread[i], NULL, passArg((int*)&n), (void*)&n))
-		printf("create thread fail");
+		if (pthread_create(&thread[i], NULL, passArg((int*)&n), &values[i]) != 0)
+			printf("create thread fail");
 		
     for (int i = 0; i < n; i++)
-	if(pthread_join(thread[i], status) != 0)
-	    printf("fail to end thread");
-	else
-	    result[i] = *(long*)status;
+		if(pthread_join(thread[i], &status) != 0)
+	    	printf("fail to end thread");
+		else
+	    	values[i] = *(long*)status;
+    
     for (int i = 0; i < n; i++)
-	printf("the value of %d is %d", i, result[i]);
+		printf("the value of %d is %d", i, values[i]);
 }
